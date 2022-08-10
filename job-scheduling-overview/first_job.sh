3eb20d7314d2f2d6b2d898afbaaacafdaa2b7067
Marco De La Pierre <marco.delapierre@gmail.com>
2022-08-10 14:27:19 +0800

    module on job scheduling overview: added job script

---
+++ job-scheduling-overview/first_job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#SBATCH --job-name=first_job
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:02:00
+
+# print date and time
+date
+
+# print host name (from the compute node) multiple times
+# wait 15 seconds in between
+for i in $(seq 1 6); do
+   echo "The hostname is: $(hostname)"
+   sleep 15s
+done

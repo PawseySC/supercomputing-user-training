e6dbb4d30b45fe93c19f824aaceb2fd8afb32575
Marco De La Pierre <marco.delapierre@gmail.com>
2022-08-11 10:30:33 +0800

    running jobs: sbatch,squeue,scancel,time limit examples

---
+++ running-jobs/submit_monitor.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#SBATCH --job-name=first_job
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:01:00
+
+# print date and time
+echo "The date is: $(date)"
+
+# print host name (from the compute node) multiple times
+# wait 15 seconds in between
+for i in $(seq 1 2); do
+   echo "The hostname is: $(hostname)"
+   sleep 15s
+done
+
+# print date and time again
+echo "The date is: $(date)"
+

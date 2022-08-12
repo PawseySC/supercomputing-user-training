6dcc48e285be308c7e65bd3065069433dce10076
Marco De La Pierre <marco.delapierre@gmail.com>
2022-08-12 16:16:22 +0800

    edits to testing-job-runs script

--- testing-job-runs/compute-scaling.sh
+++ testing-job-runs/compute-scaling.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-  
+
+input="timing-data"
+output="scaling-data"
+
+echo "# Cores Walltime[s] Relative_Walltime Speedup Efficiency Cost" >$output
+
 awk '{
+  if($1==24){nod=$1;ref=$6;refTime=$1*$6} 
+  {print $1,$6,$6/ref,ref/$6,ref/$6*nod/$1*100,$1*$6/refTime}
+}' $input >>$output
 
-if($1==24){nod=$1;ref=$6;refTime=$1*$6} 
-{print $1,$6,ref/$6,ref/$6*nod/$1*100,$1*$6/refTime}
-
-}' timing-data >scaling-data
+exit
